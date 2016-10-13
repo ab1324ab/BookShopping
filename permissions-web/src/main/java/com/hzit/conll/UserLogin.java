e98fb4c8d91981fa79f35475bce1be30a237160e
ab1324ab <435821607@qq.com>
2016-10-13 14:24:18 +0800

    方法

--- permissions-web/src/main/java/com/hzit/conll/UserLogin.java
+++ permissions-web/src/main/java/com/hzit/conll/UserLogin.java
@@ -3,5 +3,9 @@ package com.hzit.conll;
 /**
  * Created by Administrator on 2016/10/13.
  */
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserLogin {
 }
---
+++ permissions-web/src/test/java/hou/TestHou.java
@@ -0,0 +1,13 @@
+package hou;
+
+import com.sun.xml.internal.ws.api.server.InstanceResolverAnnotation;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class TestHou {
+}
