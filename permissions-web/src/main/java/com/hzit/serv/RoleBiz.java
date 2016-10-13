65ba14311e3c94659f658ff61207ee4dd152c7d3
qingguanfeng <619595145@qq.com>
2016-10-13 15:52:27 +0800

    覃上传的

--- permissions-web/src/main/java/com/hzit/serv/RoleBiz.java
+++ permissions-web/src/main/java/com/hzit/serv/RoleBiz.java
@@ -13,4 +13,31 @@ public interface RoleBiz  {
      *
      */
     public List<Role> findRoleAll();
+
+    /**
+     * 角色表,根据角色ID,查询全部的方法
+     *
+     */
+    public Role findRoleAllById(Integer roleId);
+
+    /**
+     * 角色表，新增的方法
+     */
+
+    public boolean addRole(Role role);
+
+    /**
+     * 角色表，删除的方法，根据ID删除，
+     */
+    public int deleRole(Integer roleId);
+
+
+    /**
+     * 角色表，编辑的方法，
+     */
+    public int editRole(Role role);
+
+
+
+
 }
--- permissions-web/src/main/java/com/hzit/serv/impl/RoleImpl.java
+++ permissions-web/src/main/java/com/hzit/serv/impl/RoleImpl.java
@@ -5,9 +5,11 @@ import com.hzit.dao.mapper.RoleMapper;
 import com.hzit.serv.RoleBiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/10/13.
@@ -16,8 +18,83 @@ import java.util.List;
 public class RoleImpl implements RoleBiz {
     @Autowired
     private RoleMapper roleMapper;
+
+    /**
+     * 角色表查询全部的方法
+     *
+     * @return
+     */
     @Override
     public List<Role> findRoleAll() {
         return roleMapper.searchRoleByParams(new HashMap());
+    }
+
+    /**
+     * 角色表，根据角色ID查询全部的方法
+     *
+     * @return
+     */
+    @Override
+    public Role findRoleAllById(@RequestParam(name = "roleId") Integer roleId) {
+
+        Map<String, String> p = new HashMap<String, String>();
+        p.put("roleId", roleId + "");
+
+        List<Role> list = roleMapper.searchRoleByParams(p);
+
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+        return null;
+
+    }
+
+    /**
+     * 角色表 增加角色的方法
+     * @return
+     */
+    @Override
+    public boolean addRole(Role role) {
+
+        try {
+            roleMapper.insertRole(role);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    @Override
+    public int deleRole(Integer roleId) {
+
+        try {
+
+          int num=  roleMapper.deleteRoleByRoleId(roleId);
+            return num;//num应该是1
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    /**
+     * 根据ID来编辑 角色
+
+     * @return
+     */
+    @Override
+    public int editRole(Role role) {
+
+        try {
+            int num= roleMapper.updateRole(role);
+            return num;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
     }
 }
